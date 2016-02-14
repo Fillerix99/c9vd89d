82cc8a57cb74bb0982c2faa4a1128751610c1582
Filip Nyquist <fillerix@fillerix.se>
2016-02-14 23:02:13 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,4 @@ cd /home/ubuntu
 
 sudo apt-get install supervisor xvfb fluxbox x11vnc xmacro libxtst-dev libpng++-dev -y
 git clone git://github.com/kanaka/noVNC
-npm install -g download
-npm install -g prompt
-npm install -g robotjs
 echo "DONE,NOW RUN ./run.sh, after that open a new terminal and run ./auto.sh!"
