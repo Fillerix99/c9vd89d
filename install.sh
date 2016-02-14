3bbb0989ed634c581a10f99ab8e883fa846a1d75
Filip Nyquist <fillerix@fillerix.se>
2016-02-14 23:36:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cd /home/ubuntu
-
 sudo apt-get install supervisor xvfb fluxbox x11vnc xmacro libxtst-dev libpng++-dev -y
 git clone git://github.com/kanaka/noVNC
 echo "DONE,NOW RUN ./run.sh, after that open a new terminal and run ./auto.sh!"
