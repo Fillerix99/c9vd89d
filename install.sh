79581706b5a8a1ccf73e078b4092ae703e8e6107
Filip Nyquist <fillerix@fillerix.se>
2016-02-14 20:06:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@
 
 cd /home/ubuntu
 
-sudo apt-get install supervisor xvfb fluxbox x11vnc -y
+sudo apt-get install supervisor xvfb fluxbox x11vnc xmacro -y
 git clone git://github.com/kanaka/noVNC
+npm install download
+npm install prompt
