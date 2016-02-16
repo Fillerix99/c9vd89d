19bc4f6254138f94571b72f959e809de8c5ac4f0
Filip Nyquist <fillerix@fillerix.se>
2016-02-16 17:04:30 +0100

    Revert "Replaced firefox with light!"

--- auto.sh
+++ auto.sh
@@ -3,12 +3,11 @@ export DISPLAY=:99
 cd /home/ubuntu/workspace
 npm install prompt
 npm install robotjs
-xmacroplay ":99" < light.txt
+xmacroplay ":99" < firefoxstart.txt
 node websyndic.js
-xmacroplay ":99" < light1.txt
+xmacroplay ":99" < firefoxstartwebs.txt
 node otousr.js
-xmacroplay ":99" < light2.txt
+xmacroplay ":99" < firefoxstartoto.txt
 node otopass.js
-xmacroplay ":99" < light3.txt
-echo "DONE,SCRIPT STARTED AND WORKS!:D"
-
+xmacroplay ":99" < firefoxstartoto1.txt
+cpulimit -b -l 70 -e firefox
